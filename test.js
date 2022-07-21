1c291a5efc650488091fedfe3026cdfeb95442af
HDegroote <8046565-HDegroote@users.noreply.gitlab.com>
2022-07-21 13:41:52 +0200

    [WARNING: commit with failing tests] Add tests for browser-style buffers

--- test.js
+++ test.js
@@ -17,6 +17,7 @@ tape('string', function (t) {
 
 tape('bytes', function (t) {
   test(t, encodings.bytes, [Buffer.alloc(4096), Buffer.from('hi')])
+  // TODO: Fails because it is a Buffer. Manually add Uint8Array option?
 })
 
 tape('bool', function (t) {
@@ -41,46 +42,84 @@ tape('uint64', function (t) {
 
 tape('fixed64', function (t) {
   test(t, encodings.fixed64, [Buffer.from([0, 0, 0, 0, 0, 0, 0, 1])])
+  // TODO: Fails because it is a Buffer. Manually add Uint8Array option?
 })
 
 tape('double', function (t) {
   test(t, encodings.double, [0, 2, 0.5, 0.4])
+  // TODO: Fails because writeDoubleLE is not supported by b4a
 })
 
 tape('float', function (t) {
   test(t, encodings.float, [0, 2, 0.5])
+  // TODO: Fails because writeFloatLE is not supported by b4a
 })
 
 tape('fixed32', function (t) {
   test(t, encodings.fixed32, [4, 0, 10000])
+  // TODO: Fails because writeUInt32LE is not supported by b4a
 })
 
 tape('sfixed32', function (t) {
   test(t, encodings.sfixed32, [-100, 4, 0, 142425])
+  // TODO: Fails because writeInt32LE is not supported by b4a
 })
 
 function test (t, enc, vals) {
   if (!Array.isArray(vals)) vals = [vals]
 
-  for (var i = 0; i < vals.length; i++) {
-    var val = vals[i]
-    var buf = Buffer.alloc(enc.encodingLength(val))
+  var allocFunctions = [
+    (length) => Buffer.alloc(length), // Node style
+    (length) => new Uint8Array(length) // Browser style
+  ]
 
-    enc.encode(val, buf, 0)
+  for (var allocFunction of allocFunctions) {
+    for (var i = 0; i < vals.length; i++) {
+      var val = vals[i]
+      var buf = allocFunction(enc.encodingLength(val))
 
-    t.same(enc.encode.bytes, buf.length)
-    t.same(enc.encodingLength(val), buf.length)
-    t.same(enc.decode(buf, 0), val)
-    t.same(enc.decode.bytes, buf.length)
+      enc.encode(val, buf, 0)
 
-    var anotherBuf = Buffer.alloc(enc.encodingLength(val) + 1000)
+      t.same(enc.encode.bytes, buf.length)
+      t.same(enc.encodingLength(val), buf.length)
+      t.same(enc.decode(buf, 0), val)
+      t.same(enc.decode.bytes, buf.length)
 
-    buf = enc.encode(val, anotherBuf, 10)
-    t.same(buf, anotherBuf)
-    t.ok(enc.encode.bytes < anotherBuf.length)
-    t.same(enc.decode(buf, 10, 10 + enc.encodingLength(val)), val)
-    t.ok(enc.decode.bytes < anotherBuf.length)
+      var anotherBuf = allocFunction(enc.encodingLength(val) + 1000)
+
+      buf = enc.encode(val, anotherBuf, 10)
+      t.same(buf, anotherBuf)
+      t.ok(enc.encode.bytes < anotherBuf.length)
+      t.same(enc.decode(buf, 10, 10 + enc.encodingLength(val)), val)
+      t.ok(enc.decode.bytes < anotherBuf.length)
+    }
   }
 
   t.end()
 }
+
+tape('test browser-style buffer', function (t) {
+  var enc = encodings.string
+  var val = 'value'
+  var buf = new Uint8Array(enc.encodingLength(val))
+  enc.encode(val, buf, 0)
+
+  // First elem is the length (5). Others are the encoded 'value'
+  var expectedEncodedVal = new Uint8Array([5, 118, 97, 108, 117, 101])
+  t.same(buf, expectedEncodedVal)
+
+  t.same(enc.encode.bytes, buf.length)
+  t.same(enc.encodingLength(val), buf.length)
+  t.same(enc.decode(buf, 0), val)
+  t.same(enc.decode.bytes, buf.length)
+
+  var anotherBuf = new Uint8Array(enc.encodingLength(val) + 1000)
+
+  buf = enc.encode(val, anotherBuf, 10)
+  t.same(buf, anotherBuf)
+  t.ok(enc.encode.bytes < anotherBuf.length)
+  t.same(enc.decode(buf, 10, 10 + enc.encodingLength(val)), val)
+  t.ok(enc.decode.bytes < anotherBuf.length)
+
+  t.end()
+})
